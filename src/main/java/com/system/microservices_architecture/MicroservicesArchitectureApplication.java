b07b435819a66d001c7d189f5facbb7b91cec5cc
angelariasu <angelariasus@gmail.com>
2025-10-09 21:07:58 -0500

    microservices-architecture

---
+++ src/main/java/com/system/microservices_architecture/MicroservicesArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.system.microservices_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesArchitectureApplication.class, args);
+	}
+
+}
